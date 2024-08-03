c8e965db0bfef7df43e6e605885a99435392d8d4
VeronikaKaraul <veronika.life@gmail.com>
2024-08-04 01:54:12 +0300

    added index js contact js contacts node error list

---
+++ src/contacts.js
@@ -0,0 +1,41 @@
+import fs from "fs/promises";
+import path from "path";
+
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((c) => c.id === contactId);
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((c) => c.id === contactId);
+  if (index === -1) return null;
+
+  const [removedContact] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: (Math.random() + 1).toString(36).substring(2), 
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+export { listContacts, getContactById, addContact, removeContact };
